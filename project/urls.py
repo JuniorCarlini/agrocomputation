7e5f3b214d08a1532df3c127318ed23270234059
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-05 11:17:02 -0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,22 @@
+from users.views import home
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls import handler404
+
+handler404 = 'users.views.custom_404'
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
+
+    # Home
+    path('', home, name='home'),
+
+    # Users
+    path("users/", include("users.urls")),
+
+    # Modules
+    path("irrigation/", include("irrigation.urls")),
+    path("station/", include("station.urls")),
 ]
