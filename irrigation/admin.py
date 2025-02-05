0b3a348023384e8f2575178c5123ab2f0697ba08
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-05 11:19:47 -0300

    Update admin.py

--- irrigation/admin.py
+++ irrigation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SolenoidState, DataCollection, FlowRate, WaterUsage, Configuration, StatusFertil , ConfigFertil, StoricFertil, TimeFerti
 
-# Register your models here.
+admin.site.register(SolenoidState)
+admin.site.register(DataCollection)
+admin.site.register(FlowRate)
+admin.site.register(WaterUsage)
+admin.site.register(Configuration)
+admin.site.register(StatusFertil)
+admin.site.register(ConfigFertil)
+admin.site.register(StoricFertil)
+admin.site.register(TimeFerti)
