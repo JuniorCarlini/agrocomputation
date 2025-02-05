b13db559b52eaf82568a46d807c86102b6731f74
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-05 11:19:56 -0300

    Update views.py

--- station/views.py
+++ station/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import WeatherData
+from rest_framework import status
+from rest_framework.response import Response
+from project.decorators import token_required
+from .serializers import WeatherDataSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['POST'])
+@token_required
+def collect_weather_data(request):
+    serializer = WeatherDataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message": "Weather data recorded successfully"
+        }, status=status.HTTP_201_CREATED)
+    return Response({
+        "error": serializer.errors
+    }, status=status.HTTP_400_BAD_REQUEST)
