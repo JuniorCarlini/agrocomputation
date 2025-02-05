346c322f02aa724325c0d0954b50b8f89db4b7ca
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-05 16:18:20 -0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
+from django.urls import path
 from users.views import login_view
-from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
-
