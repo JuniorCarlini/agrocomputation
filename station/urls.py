04b905065a22fc6004d7673416d2090c23801634
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-07 13:59:01 -0300

    Update urls.py

--- station/urls.py
+++ station/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import collect_weather_data
+from .views import collect_weather_data, dashboard_station_view
 
 urlpatterns = [
     # Api
     path('api/post-weather-data/', collect_weather_data, name='collect_weather'),
 
     # Templates
+    path ('', dashboard_station_view, name='dashboard_station'),
+    
+
 ]
