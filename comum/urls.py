d2d4ca98c3cc15c88a90f1cfd82d169d5554e0e3
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-07 13:52:54 -0300

    Update urls.py

--- comum/urls.py
+++ comum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from comum.views import apresentation_view
+from comum.views import apresentation_view, configuration_view
 
 urlpatterns = [
     path('', apresentation_view, name='apresentation'),
+    path('configuration/', configuration_view, name='configuration'),
 ]
