57d1a8e45721fa5deb7a26bf1127fd95760a7a51
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2025-02-26 16:50:25 -0300

    Update urls.py

--- station/urls.py
+++ station/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from .views import collect_weather_data, dashboard_station_view
+from . import views
 
 urlpatterns = [
     # Api
-    path('api/post-weather-data/', collect_weather_data, name='collect_weather'),
+    path('api/post-weather-data/', views.collect_weather_data, name='collect_weather'),
 
     # Templates
-    path ('', dashboard_station_view, name='dashboard_station'),
-    
+    path ('', views.dashboard_station_view, name='dashboard_station'),
+    path('dht-temperature-chart/', views.dht_temperature_chart_view, name='dht_temperature_chart'),
+    path('bmp-temperature-chart/', views.bmp_temperature_chart_view, name='bmp_temperature_chart'),
+    path('air-humidity-chart/', views.humidity_chart_view, name='air_humidity_chart'),
+    path('pressure-chart/', views.pressure_chart_view, name='pressure_chart'),
+    path('wind-speed-chart/', views.wind_speed_chart_view, name='wind_speed_chart'),
+    path('wind-direction-chart/', views.wind_direction_chart_view, name='wind_direction_chart'),
+    path('rainfall-chart/', views.rainfall_chart_view, name='rainfall_chart'),
+    path('temperature-comparison-chart/', views.temperature_comparison_chart_view, name='temperature_comparison_chart'),
+
 
 ]
